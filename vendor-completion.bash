11882d2011909aeda204ab717edfcf04ee21c4da
Soichiro Shishido <shishidosoichiro@gmail.com>
2019-05-20 23:39:32 +0900

    completion

--- vendor-completion.bash
+++ vendor-completion.bash
@@ -1,26 +1,51 @@
 # source ./vendor-completion.bash
 __vendor_comp_func () {
+  local vendor=${COMP_WORDS[0]}
+  local subcommand1=${COMP_WORDS[1]}
+  local subcommand2=${COMP_WORDS[2]}
   local cur=${COMP_WORDS[COMP_CWORD]}
   local prev=${COMP_WORDS[COMP_CWORD-1]}
-  VENDOR=vendor
-  case "$prev" in
-    "vendor"|"./vendor"|"vendor.exe"|"./vendor.exe")
-      COMPREPLY=($(compgen -W "bin env home install latest ls search uninstall versions manager root util" -- "${cur}"))
-    ;;
-    "manager")
-      COMPREPLY=($(compgen -W "exec pull init" -- "${cur}"))
-    ;;
-    "root")
-      COMPREPLY=($(compgen -W "crobber pull" -- "${cur}"))
-    ;;
-    "bin"|"env"|"home"|"install"|"latest"|"ls"|"search"|"uninstall"|"versions"|"exec"|"pull"|"init"|"crobber"|"pull")
-      apps=$($VENDOR ls)
-      COMPREPLY=($(compgen -W "$apps" -- "${cur}"))
-    ;;
-    *)
-      echo "none of the above"
-    ;;
-  esac
+  if [ $COMP_CWORD = 0 ]; then
+    continue;
+  elif [ $COMP_CWORD = 1 ]; then
+    COMPREPLY=($(compgen -W "bin env home install latest ls search uninstall versions manager root util" -- "${cur}"))
+  elif [ $COMP_CWORD = 2 ]; then
+    case "$subcommand1" in
+      "manager")
+        COMPREPLY=($(compgen -W "exec pull init" -- "${cur}"))
+      ;;
+      "root")
+        COMPREPLY=($(compgen -W "crobber pull" -- "${cur}"))
+      ;;
+      "bin"|"env"|"home"|"install"|"latest"|"ls"|"search"|"uninstall"|"versions")
+        apps=$($vendor ls)
+        COMPREPLY=($(compgen -W "$apps" -- "${cur}"))
+      ;;
+    esac
+  else
+    case "$subcommand1" in
+      "manager")
+        case "$subcommand2" in
+          "exec"|"pull"|"init")
+            apps=$($vendor ls)
+            COMPREPLY=($(compgen -W "$apps" -- "${cur}"))
+          ;;
+        esac
+      ;;
+      "root")
+        case "$subcommand2" in
+          "crobber"|"pull")
+            apps=$($vendor ls)
+            COMPREPLY=($(compgen -W "$apps" -- "${cur}"))
+          ;;
+        esac
+      ;;
+      "bin"|"env"|"home"|"install"|"latest"|"ls"|"search"|"uninstall"|"versions")
+        apps=$($vendor ls)
+        COMPREPLY=($(compgen -W "$apps" -- "${cur}"))
+      ;;
+    esac
+  fi;
 }
 complete -F __vendor_comp_func vendor
 complete -F __vendor_comp_func ./vendor
